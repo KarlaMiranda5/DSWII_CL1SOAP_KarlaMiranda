d0be6b0cae2ee25b24f158b4d99e3cebb499fecc
KarlaMiranda5 <i201720871@cibertec.edu.pe>
2023-09-30 09:53:49 -0500

    Initial commit

---
+++ src/test/java/edu/pe/cibertec/apisoapcibertec/ApiSoapCibertecApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.apisoapcibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSoapCibertecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
