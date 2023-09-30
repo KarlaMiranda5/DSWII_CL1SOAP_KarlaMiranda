7a2e6cf3462f976b36abceafc9828077065891e0
KarlaMiranda5 <i201720871@cibertec.edu.pe>
2023-09-30 09:53:49 -0500

    Initial commit

---
+++ src/main/java/edu/pe/cibertec/apisoapcibertec/ApiSoapCibertecApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.apisoapcibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSoapCibertecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSoapCibertecApplication.class, args);
+	}
+
+}
